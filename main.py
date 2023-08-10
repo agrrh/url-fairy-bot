612da51942bb5fd1fa32176562f823b93ad58092
Vladimir @nett00n Budylnikov <git@nett00n.org>
2023-08-10 08:12:48 +0000

    follow link redirects

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from dotenv import load_dotenv
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -11,10 +12,21 @@ CHAT_ID = int(os.getenv("CHAT_ID"))
 def start(update, context):
     update.message.reply_text("Hello! I'm your bot. Send me a message!")
 
+def follow_redirects(url):
+    response = requests.head(url, allow_redirects=True)
+    return response.url
+
 def forward_message(update, context):
     if update.message.from_user.id == USER_ID:
-        context.bot.send_message(chat_id=CHAT_ID, text=update.message.text)
-        update.message.reply_text("Your message has been forwarded to the selected channel.")
+        message_text = update.message.text
+
+        if message_text.startswith("http") or message_text.startswith("www"):
+            clean_url = follow_redirects(message_text)
+            context.bot.send_message(chat_id=CHAT_ID, text=clean_url)
+            update.message.reply_text("Cleaned link has been forwarded to the selected channel.")
+        else:
+            context.bot.send_message(chat_id=CHAT_ID, text=message_text)
+            update.message.reply_text("Your message has been forwarded to the selected channel.")
     else:
         update.message.reply_text("Sorry, you are not authorized to use this bot.")
 
